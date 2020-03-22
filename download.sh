6bd6ef8b88b0d60cff06cc5dd4a1bd73421805bd
mac2 <mac2@example.com>
2020-03-22 18:58:01 +0900

    fix

--- download.sh
+++ download.sh
@@ -9,7 +9,7 @@ dir=$(mktemp -d)
 
 (
 cd $dir
-seq 0 0 | xargs -n1 -P10 bash -c 'curl -O -s https://raw.githubusercontent.com/contribu/ghlargefiletest/master/$0.xz > $0'
+seq 0 0 | xargs -n1 -P10 bash -c 'curl -O -s https://raw.githubusercontent.com/contribu/ghlargefiletest/master/$0.xz | xz -d > $0'
 cat $(seq 0 0) > output
 )
 
